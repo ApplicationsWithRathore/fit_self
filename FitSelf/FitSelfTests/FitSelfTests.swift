e257175f781ba4a7349e1524314167d24ae4d27e
Jitendra Rathore <jitendra.rathore@nihilent.com>
2025-06-21 11:52:28 +0530

    Xcode setup

---
+++ FitSelf/FitSelfTests/FitSelfTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitSelfTests.swift
+//  FitSelfTests
+//
+//  Created by Jitendra Rathore on 21/06/25.
+//
+
+import Testing
+@testable import FitSelf
+
+struct FitSelfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
